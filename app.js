990b1a58fadf5ed1b063168d6f51e7d452aa5123
pedrodias92 <pedro.dias92@hotmail.com>
2019-03-11 19:52:06 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const title = 'My First React Element!'
+const desc = 'I just learned how to create a React node and render it into the Dom'
+const myTitleID = 'main-title'
+const name = 'Guil'
+
+const header = (
+    <header>
+    {/* Comments */}
+        <h1 id={myTitleID}>{name}'s' First React Element!</h1>
+        <p className="main-desc">{desc}</p>
+    </header>
+)
+
+ReactDOM.render(
+    header,  
+    document.getElementById('root')
+)
